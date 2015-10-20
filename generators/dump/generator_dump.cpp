e1e50db189779030a96d565f18c03d1d155200d6
Chris Burel <chrisburel@gmail.com>
2015-10-19 17:35:33 -0700

    Refactor dump generator to dump everything.

--- generators/dump/generator_dump.cpp
+++ generators/dump/generator_dump.cpp
@@ -24,13 +24,70 @@
 
 #include <iostream>
 
+#include <QDebug>
+
 extern "C" Q_DECL_EXPORT
 void generate()
 {
-    foreach (const QString& className, classes.keys()) {
-        std::cout << qPrintable(className);
-        if (classes[className].isForwardDecl())
-            std::cout << " [forward declaration]";
-        std::cout << std::endl;
+//extern GENERATOR_EXPORT QHash<QString, Class> classes;
+//extern GENERATOR_EXPORT QHash<QString, Typedef> typedefs;
+//extern GENERATOR_EXPORT QHash<QString, Enum> enums;
+//extern GENERATOR_EXPORT QHash<QString, Function> functions;
+//extern GENERATOR_EXPORT QHash<QString, GlobalVar> globals;
+//extern GENERATOR_EXPORT QHash<QString, Type> types;
+
+    std::map<QString, Class> sortedClasses;
+    foreach (const QString& key, classes.keys()) {
+        sortedClasses[key] = classes[key];
+    }
+
+    std::map<QString, Typedef> sortedTypedefs;
+    foreach (const QString& key, typedefs.keys()) {
+        sortedTypedefs[key] = typedefs[key];
+    }
+
+    std::map<QString, Enum> sortedEnums;
+    foreach (const QString& key, enums.keys()) {
+        sortedEnums[key] = enums[key];
+    }
+
+    std::map<QString, Function> sortedFunctions;
+    foreach (const QString& key, functions.keys()) {
+        sortedFunctions[key] = functions[key];
+    }
+
+    std::map<QString, Type> sortedTypes;
+    foreach (const QString& key, types.keys()) {
+        sortedTypes[key] = types[key];
+    }
+
+    qDebug() << "Classes:";
+    for (const std::pair<const QString, Class>& item : sortedClasses) {
+        const Class& klass = item.second;
+        qDebug() << "    " << klass;
+    }
+
+    qDebug() << "Typedefs:";
+    for (const std::pair<const QString, Typedef>& item : sortedTypedefs) {
+        const Typedef& tdef = item.second;
+        qDebug() << "    " << tdef;
+    }
+
+    qDebug() << "Enums:";
+    for (const std::pair<const QString, Enum>& item : sortedEnums) {
+        const Enum& e = item.second;
+        qDebug() << "    " << e;
+    }
+
+    qDebug() << "Functions:";
+    for (const std::pair<const QString, Function>& item : sortedFunctions) {
+        const Function& function = item.second;
+        qDebug() << "    " << function;
+    }
+
+    qDebug() << "Types:";
+    for (const std::pair<const QString, Type>& item : sortedTypes) {
+        const Type& type = item.second;
+        qDebug() << "    " << type;
     }
 }
